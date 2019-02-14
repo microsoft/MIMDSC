8af4bf11866a7c580f2e3f992d029eeb5de3fb06
Craig Martin <cmart@microsoft.com>
2019-02-13 19:55:51 -0800

    Updating MimSyncExportAttributeFlowRule with Pester tests

--- MimSyncDsc/Tests/MimSyncExportAttributeFlowRule.tests.ps1
+++ MimSyncDsc/Tests/MimSyncExportAttributeFlowRule.tests.ps1
@@ -1,129 +1,206 @@
-﻿
+
 Get-MimSyncServerXml -Path (Get-MimSyncConfigCache) -Force
 
 $dscResource = Get-DscResource -Module MimSyncDsc -Name MimSyncExportAttributeFlowRule
 
-ipmo $dscResource.Path -Force
+Import-Module -Name $dscResource.Path -Force
 
-#region TestMimSyncExportAttributeFlowRule - Direct Mapping
-Configuration TestMimSyncExportAttributeFlowRule 
-{ 
-    Import-DscResource -ModuleName MimSyncDsc
+Describe 'MimSyncExportAttributeFlowRule - calling Test-TargetResource Directly'{
+    It 'Direct EAF Rule - desired state' {
 
-    Node (hostname) 
-    { 
-        ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
-        {
-           ManagementAgentName    = 'TinyHR'
-           MVObjectType           = 'SyncObject'
-           CDAttribute            = 'JobTitle'
-           CDObjectType           = 'person'
-           Type                   = 'direct-mapping'
-           SrcAttribute           = 'Title'
-           SuppressDeletions      = $false
-           Ensure                 = 'Present'
-        }
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute JobTitle -CDObjectType person -Type 'direct-mapping' -SrcAttribute Title -SuppressDeletions $false -Ensure Present -Verbose
+
+        $dscResult | Should be True
     }
-} 
 
-TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
-Start-DscConfiguration                    -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose   
+    It 'Scripted EAF Rule - desired state' {
 
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute JobTitle -CDObjectType person -Type 'direct-mapping' -SrcAttribute Title -SuppressDeletions $false -Ensure Present -Verbose
-Get-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute JobTitle -CDObjectType person -Type 'direct-mapping' -Verbose
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute HireDate -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'object-id' -ScriptContext 'cd.person:HireDate<-mv.SyncObject:<object-id>' -SuppressDeletions $true -Ensure Present -Verbose
 
-#endregion
-
-#region TestMimSyncExportAttributeFlowRule - Scripted Mapping with Intrinsic SrcAttribute
-Configuration TestMimSyncExportAttributeFlowRule 
-{ 
-    Import-DscResource -ModuleName MimSyncDsc
-
-    Node (hostname) 
-    { 
-        ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
-        {
-           ManagementAgentName    = 'TinyHR'
-           MVObjectType           = 'SyncObject'
-           CDAttribute            = 'HireDate'
-           CDObjectType           = 'person'
-           Type                   = 'scripted-mapping'
-           SrcAttribute           = 'object-id'
-           ScriptContext          = 'cd.person:HireDate<-mv.SyncObject:<object-id>'
-           SuppressDeletions      = $true
-           Ensure                 = 'Present'
-        }
+        $dscResult | Should be True
     }
-} 
 
-TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
-Start-DscConfiguration                    -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose   
+    It 'Scripted EAF Rule - multiple source attributes - desired state' {
 
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute HireDate -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'object-id' -ScriptContext 'cd.person:HireDate<-mv.SyncObject:<object-id>' -SuppressDeletions $true -Ensure Present -Verbose
-Set-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute HireDate -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'object-id' -ScriptContext 'cd.person:HireDate<-mv.SyncObject:<object-id>' -SuppressDeletions $true -Ensure Present -Verbose
-Get-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute HireDate -CDObjectType person -Type 'scripted-mapping' -Verbose
-#endregion
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $false -Ensure Present -Verbose
 
-#region TestMimSyncExportAttributeFlowRule - Scripted Mapping
-Configuration TestMimSyncExportAttributeFlowRule 
-{ 
-    Import-DscResource -ModuleName MimSyncDsc
-
-    Node (hostname) 
-    { 
-        ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
-        {
-           ManagementAgentName    = 'TinyHR'
-           MVObjectType           = 'SyncObject'
-           CDAttribute            = 'Initial'
-           CDObjectType           = 'person'
-           Type                   = 'scripted-mapping'
-           SrcAttribute           = 'Alias','FirstName','LastName'
-           ScriptContext          = 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName'
-           SuppressDeletions      = $false
-           Ensure                 = 'Present'
-        }
+        $dscResult | Should be True
     }
-} 
 
-TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
-Start-DscConfiguration                    -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose   
+    It 'Scripted EAF Rule - multiple source attributes - incorrect suppress-deletion' {
 
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $false -Ensure Present -Verbose
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $true -Ensure Present -Verbose
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName','MiddleName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $false -Ensure Present -Verbose
-Set-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $true -Ensure Present -Verbose
-Get-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -Verbose
-#endregion
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $true -Ensure Present -Verbose
 
-#region TestMimSyncExportAttributeFlowRule - Constant Mapping
-Configuration TestMimSyncExportAttributeFlowRule 
-{ 
-    Import-DscResource -ModuleName MimSyncDsc
-
-    Node (hostname) 
-    { 
-        ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
-        {
-           ManagementAgentName    = 'TinyHR'
-           MVObjectType           = 'SyncObject'
-           CDAttribute            = 'Status'
-           CDObjectType           = 'person'
-           Type                   = 'constant-mapping'
-           ConstantValue          = 'AlwaysThisLate'
-           SuppressDeletions      = $true
-           Ensure                 = 'Present'
-        }
+        $dscResult | Should be False
     }
-} 
 
-TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
-Start-DscConfiguration                    -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose   
+    It 'Scripted EAF Rule - multiple source attributes - incorrect src-attribute' {
 
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Status -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AlwaysThisLate' -Ensure Present -Verbose
-Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute FooBar -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AllTheBaseAre ' -Ensure Present -Verbose
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Initial -CDObjectType person -Type 'scripted-mapping' -SrcAttribute 'Alias','FirstName','LastName','MiddleName' -ScriptContext 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName' -SuppressDeletions $false -Ensure Present -Verbose
 
-Set-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Status -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AlwaysThisLate' -Ensure Present -Verbose
-Get-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Status -CDObjectType person -Type 'constant-mapping' -Verbose
-Get-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute 3tatus -CDObjectType person -Type 'constant-mapping' -Verbose
-#endregion
+        $dscResult | Should be False
+    }
+
+    It 'Constant EAF Rule - desired state' {
+
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Status -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AlwaysThisLate' -Ensure Present -Verbose
+
+        $dscResult | Should be True
+    }
+    
+    It 'Constant EAF Rule - incorrect contant-value' {
+
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Status -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AllTheBaseAre ' -Ensure Present -Verbose
+
+        $dscResult | Should be False
+    }
+
+    It 'Constant EAF Rule - missing EAF rule' {
+
+        $dscResult = Test-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute Foo -CDObjectType person -Type 'constant-mapping' -ConstantValue 'AllTheBaseAre ' -Ensure Present -Verbose
+
+        $dscResult | Should be False
+    }
+}
+
+
+Describe 'MimSyncExportAttributeFlowRule - calling Get-TargetResource Directly'{
+    It 'Direct EAF Rule - desired state' {
+
+        $dscResult = Get-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute JobTitle -CDObjectType person -Type 'direct-mapping' -Verbose
+
+        $dscResult['SrcAttribute'] | Should be 'Title'        
+    }
+
+    It 'Scripted EAF Rule - desired state' {
+
+        $dscResult = Get-TargetResource  -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute HireDate -CDObjectType person -Type 'scripted-mapping' -Verbose
+
+        $dscResult['SrcAttribute'] | Should be 'object-id'        
+    }
+
+    It 'Direct EAF Rule - undesirable state' {
+        {Get-TargetResource -ManagementAgentName TinyHR -MVObjectType SyncObject -CDAttribute JobTitles -CDObjectType person -Type 'direct-mapping' -Verbose} | Should Throw
+    }
+}
+
+
+Describe 'MimSyncExportAttributeFlowRule - using the Local Configuration Manager'{
+    It 'Direct EAF Rule - desired state' {
+        Configuration TestMimSyncExportAttributeFlowRule 
+        { 
+            Import-DscResource -ModuleName MimSyncDsc
+
+            Node (hostname) 
+            { 
+                ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
+                {
+                   ManagementAgentName    = 'TinyHR'
+                   MVObjectType           = 'SyncObject'
+                   CDAttribute            = 'JobTitle'
+                   CDObjectType           = 'person'
+                   Type                   = 'direct-mapping'
+                   SrcAttribute           = 'Title'
+                   SuppressDeletions      = $false
+                   Ensure                 = 'Present'
+                }
+            }
+        } 
+
+        TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
+        Start-DscConfiguration       -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose 
+
+        $dscStatus = Get-DscConfigurationStatus
+
+        $dscStatus.Status | Should Be 'Success'
+    }
+
+    It 'Scripted EAF Rule - desired state' {
+        Configuration TestMimSyncExportAttributeFlowRule 
+        { 
+            Import-DscResource -ModuleName MimSyncDsc
+
+            Node (hostname) 
+            { 
+                ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
+                {
+                   ManagementAgentName    = 'TinyHR'
+                   MVObjectType           = 'SyncObject'
+                   CDAttribute            = 'HireDate'
+                   CDObjectType           = 'person'
+                   Type                   = 'scripted-mapping'
+                   SrcAttribute           = 'object-id'
+                   ScriptContext          = 'cd.person:HireDate<-mv.SyncObject:<object-id>'
+                   SuppressDeletions      = $true
+                   Ensure                 = 'Present'
+                }
+            }
+        } 
+
+        TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
+        Start-DscConfiguration       -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose 
+
+        $dscStatus = Get-DscConfigurationStatus
+
+        $dscStatus.Status | Should Be 'Success'
+    }
+
+    It 'Scripted EAF Rule - multiple source attributes - desired state' {
+        Configuration TestMimSyncExportAttributeFlowRule 
+        { 
+            Import-DscResource -ModuleName MimSyncDsc
+
+            Node (hostname) 
+            { 
+                ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
+                {
+                   ManagementAgentName    = 'TinyHR'
+                   MVObjectType           = 'SyncObject'
+                   CDAttribute            = 'Initial'
+                   CDObjectType           = 'person'
+                   Type                   = 'scripted-mapping'
+                   SrcAttribute           = 'Alias','FirstName','LastName'
+                   ScriptContext          = 'cd.person:Initial<-mv.SyncObject:Alias,FirstName,LastName'
+                   SuppressDeletions      = $false
+                   Ensure                 = 'Present'
+                }
+            }
+        } 
+
+        TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
+        Start-DscConfiguration       -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose 
+
+        $dscStatus = Get-DscConfigurationStatus
+
+        $dscStatus.Status | Should Be 'Success'
+    }
+
+    It 'Constant EAF Rule - desired state' {
+        Configuration TestMimSyncExportAttributeFlowRule 
+        { 
+            Import-DscResource -ModuleName MimSyncDsc
+
+            Node (hostname) 
+            { 
+                ExportAttributeFlowRule TestMimSyncExportAttributeFlowRule
+                {
+                   ManagementAgentName    = 'TinyHR'
+                   MVObjectType           = 'SyncObject'
+                   CDAttribute            = 'Status'
+                   CDObjectType           = 'person'
+                   Type                   = 'constant-mapping'
+                   ConstantValue          = 'AlwaysThisLate'
+                   SuppressDeletions      = $true
+                   Ensure                 = 'Present'
+                }
+            }
+        } 
+
+        TestMimSyncExportAttributeFlowRule -OutputPath "$env:TEMP\TestMimSyncExportAttributeFlowRule"
+        Start-DscConfiguration       -Path "$env:TEMP\TestMimSyncExportAttributeFlowRule" -Force -Wait -Verbose 
+
+        $dscStatus = Get-DscConfigurationStatus
+
+        $dscStatus.Status | Should Be 'Success'
+    }
+}
