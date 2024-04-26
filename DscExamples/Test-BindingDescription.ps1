c23e659ad2386086998b69d530e23c9aec00586f
craig-martin <craigmartin@live.com>
2024-04-26 19:44:55 +0000

    adds BindingDescription DSC resource

---
+++ DscExamples/Test-BindingDescription.ps1
@@ -0,0 +1,242 @@
+﻿Set-Location c:\MimDsc
+
+#region Call the Set and Test methods directly - allows for setting a breakpoint for debuggering
+ipmo MimDsc -Force
+ipmo 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcBindingDescription\MimSvcBindingDescription.psm1' -Force
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcBindingDescription\MimSvcBindingDescription.psm1'
+
+$displayName = "Binding$(Get-Random -Minimum 100 -Maximum 999)"
+$resourceProperties = @{
+    DataType                    = 'String'
+    Description					= 'initial description'
+    DisplayName					= $displayName
+    #Localizable                 = ''
+    Name             			= $displayName
+    Multivalued                 = $false
+    #UsageKeyword            	= 'Microsoft.ResourceManagement.PasswordReset'
+    Ensure						= 'Present'
+}
+
+$resourceProperties = @{
+	BoundAttributeType = 'AccountName'
+	BoundObjectType    = 'Person'
+	Description        = 'User''s log on name'
+	DisplayName        = 'Account Name'
+	Required           = $False
+	StringRegex        = '^[^"/\\[\]:;|=,+/*?<>]{1,64}$'
+	UsageKeyword       = 'Microsoft.ResourceManagement.OfficeIntegration','Microsoft.ResourceManagement.PasswordReset','Microsoft.ResourceManagement.PortalClient'
+	DependsOn          = '[cFimAttributeTypeDescription]AccountName','[cFimObjectTypeDescription]Person'
+	Ensure             = 'Present'
+	Credential         = $fimCredential
+}
+
+Test-MimSvcTargetResource -ObjectType BindingDescription -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+Set-MimSvcTargetResource -ObjectType BindingDescription -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+#endregion
+
+
+#region: Test for an existing Binding
+Search-Resources -XPath "/BindingDescription[BoundAttributeType=/AttributeTypeDescription[Name='DisplayName'] and BoundObjectType=/ObjectTypeDescription[Name='Person']]" -ExpectedObjectType BindingDescription
+
+Configuration TestBinding 
+{ 
+
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription $displayName
+        {
+            BoundAttributeType          = 'DisplayName'
+            BoundObjectType             = 'Person'            
+            DisplayName					= 'Display Name'
+            Required                    = $false
+            UsageKeyword                = 'Microsoft.ResourceManagement.OfficeIntegration','Microsoft.ResourceManagement.PortalClient'
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestBinding" -Force
+#endregion
+
+#region: New Binding
+$randomNumber = Get-Random -Minimum 100 -Maximum 999
+Configuration TestBinding 
+{ 
+
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription "Binding$randomNumber"
+        {
+            BoundAttributeType          = "Attribute$randomNumber"
+            BoundObjectType             = 'Person'
+            Description					= 'initial description'
+            DisplayName					= "Binding$randomNumber"
+            Required                    = $false
+            Ensure						= 'Present'
+            DependsOn                   = "[AttributeTypeDescription]Attribute$randomNumber"
+        }
+
+        AttributeTypeDescription "Attribute$randomNumber"
+        {
+            DataType                    = 'String'
+            Description					= 'initial description'
+            DisplayName					= "Attribute$randomNumber"
+            Localizable                 = $false
+            Name             			= "Attribute$randomNumber"
+            Multivalued                 = $false
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestBinding"
+#endregion
+
+#region: New Binding with StringRegEx
+$randomNumber = Get-Random -Minimum 100 -Maximum 999
+Configuration TestBinding 
+{ 
+
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription "Binding$randomNumber"
+        {
+            BoundAttributeType          = "Attribute$randomNumber"
+            BoundObjectType             = 'Person'
+            Description					= 'initial description'
+            DisplayName					= "Binding$randomNumber"
+            Required                    = $false
+            StringRegEx                 = 'foo'
+            Ensure						= 'Present'
+            DependsOn                   = "[AttributeTypeDescription]Attribute$randomNumber"
+        }
+
+        AttributeTypeDescription "Attribute$randomNumber"
+        {
+            DataType                    = 'String'
+            Description					= 'initial description'
+            DisplayName					= "Attribute$randomNumber"
+            Localizable                 = $false
+            Name             			= "Attribute$randomNumber"
+            Multivalued                 = $false
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestBinding" -Force
+#endregion
+
+#region: Update Binding: Description
+$randomNumber = Get-Random -Minimum 100 -Maximum 999
+Configuration TestBinding 
+{ 
+
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription "Binding$randomNumber"
+        {
+            BoundAttributeType          = "Attribute$randomNumber"
+            BoundObjectType             = 'Person'
+            Description					= 'initial description'
+            DisplayName					= "Binding$randomNumber"
+            Required                    = $false
+            Ensure						= 'Present'
+            DependsOn                   = "[AttributeTypeDescription]Attribute$randomNumber"
+        }
+
+        AttributeTypeDescription "Attribute$randomNumber"
+        {
+            DataType                    = 'String'
+            Description					= 'initial description'
+            DisplayName					= "Attribute$randomNumber"
+            Localizable                 = $false
+            Name             			= "Attribute$randomNumber"
+            Multivalued                 = $false
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestBinding" -Force
+
+Configuration TestBinding 
+{ 
+
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription "Binding$randomNumber"
+        {
+            BoundAttributeType          = "Attribute$randomNumber"
+            BoundObjectType             = 'Person'
+            Description					= 'updated description'
+            DisplayName					= "Binding$randomNumber"
+            Required                    = $false
+            Ensure						= 'Present'
+            DependsOn                   = "[AttributeTypeDescription]Attribute$randomNumber"
+        }
+
+        AttributeTypeDescription "Attribute$randomNumber"
+        {
+            DataType                    = 'String'
+            Description					= 'initial description'
+            DisplayName					= "Attribute$randomNumber"
+            Localizable                 = $false
+            Name             			= "Attribute$randomNumber"
+            Multivalued                 = $false
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestBinding" -Force
+#endregion
+
+#region: Test for an existing Binding that is not unique
+Configuration TestBinding 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        BindingDescription PersonAccountName
+        {
+	        BoundAttributeType = 'AccountName'
+	        BoundObjectType    = 'Person'
+	        Description        = 'User''s log on name'
+	        DisplayName        = 'Account Name'
+	        Required           = $False
+	        StringRegex        = '^[^"/\\[\]:;|=,+/*?<>]{1,64}$'
+	        UsageKeyword       = 'Microsoft.ResourceManagement.OfficeIntegration','Microsoft.ResourceManagement.PasswordReset','Microsoft.ResourceManagement.PortalClient'
+	        #DependsOn          = '[cFimAttributeTypeDescription]AccountName','[cFimObjectTypeDescription]Person'
+	        Ensure             = 'Present'
+        }
+    } 
+} 
+
+TestBinding
+
+Start-DscConfiguration -Wait -Verbose -Path "c:\MimDsc\TestBinding" -Force
+#endregion
---
+++ DscResources/MimSvcBindingDescription/MimSvcBindingDescription.psm1
@@ -0,0 +1,179 @@
+﻿function Get-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Collections.Hashtable])]
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$BoundAttributeType,
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$BoundObjectType
+	)
+
+	#Write-Verbose "Use this cmdlet to deliver information about command processing."
+
+	#Write-Debug "Use this cmdlet to write debug information while troubleshooting."
+
+
+	<#
+	$returnValue = @{
+		Required = [System.Boolean]
+		BoundAttributeType = [System.String]
+		CreatedTime = [System.DateTime]
+		Creator = [System.String]
+		DeletedTime = [System.DateTime]
+		Description = [System.String]
+		DetectedRulesList = [System.String[]]
+		DisplayName = [System.String]
+		ExpectedRulesList = [System.String[]]
+		ExpirationTime = [System.DateTime]
+		IntegerMaximum = [System.UInt64]
+		IntegerMinimum = [System.UInt64]
+		Locale = [System.String]
+		Localizable = [System.Boolean]
+		MVObjectID = [System.String]
+		ObjectID = [System.String]
+		ResourceTime = [System.DateTime]
+		BoundObjectType = [System.String]
+		ObjectType = [System.String]
+		StringRegex = [System.String]
+		UsageKeyword = [System.String[]]
+		Credential = [System.Management.Automation.PSCredential]
+		Ensure = [System.String]
+	}
+
+	$returnValue
+	#>
+}
+
+
+function Set-TargetResource
+{
+	[CmdletBinding()]
+	param
+	(
+		[System.Boolean]
+		$Required,
+
+        [parameter(Mandatory = $true)]
+		[System.String]
+		$BoundAttributeType,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.UInt64]
+		$IntegerMaximum,
+
+		[System.UInt64]
+		$IntegerMinimum,
+
+		[System.String]
+		$Locale,
+
+		[System.Boolean]
+		$Localizable,
+
+		[System.String]
+		$MVObjectID,
+
+        [parameter(Mandatory = $true)]
+		[System.String]
+		$BoundObjectType,
+
+		[System.String]
+		$StringRegex,
+
+		[System.String[]]
+		$UsageKeyword,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Set-MimSvcTargetResource -ObjectType BindingDescription -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+function Test-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Boolean])]
+	param
+	(
+		[System.Boolean]
+		$Required,
+
+        [parameter(Mandatory = $true)]
+		[System.String]
+		$BoundAttributeType,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.UInt64]
+		$IntegerMaximum,
+
+		[System.UInt64]
+		$IntegerMinimum,
+
+		[System.String]
+		$Locale,
+
+		[System.Boolean]
+		$Localizable,
+
+		[System.String]
+		$MVObjectID,
+
+        [parameter(Mandatory = $true)]
+		[System.String]
+		$BoundObjectType,
+
+		[System.String]
+		$StringRegex,
+
+		[System.String[]]
+		$UsageKeyword,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Test-MimSvcTargetResource -ObjectType BindingDescription -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+Export-ModuleMember -Function *-TargetResource
