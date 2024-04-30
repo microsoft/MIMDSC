c7b7dab10e326f948eb0351522f89aa9eefbe663
craig-martin <craigmartin@live.com>
2024-04-30 18:15:59 +0000

    adds PortalUIConfiguration DSC resource

---
+++ DscExamples/Test-PortalUIConfiguration.ps1
@@ -0,0 +1,152 @@
+﻿Set-Location c:\MimDsc
+
+#region Call the Set and Test methods directly - allows for setting a breakpoint for debuggering
+Search-Resources -XPath "/PortalUIConfiguration" -ExpectedObjectType PortalUIConfiguration -MaxResults 5
+
+ipmo MimDsc -Force
+ipmo 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcPortalUIConfiguration\MimSvcPortalUIConfiguration.psm1' -Force
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcPortalUIConfiguration\MimSvcPortalUIConfiguration.psm1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Test-MimSvcTargetResource.ps1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Set-MimSvcTargetResource.ps1'
+
+$resourceProperties = @{
+	BrandingLeftImage    = '~/_layouts/images/MSILM2/msit-idweb1.png'
+	BrandingRightImage   = '~/_layouts/images/MSILM2/banner.png'
+	DisplayName          = 'Portal Configuration'
+	IsConfigurationType  = $True
+	ListViewCacheTimeOut = '120'
+	ListViewPageSize     = '50'
+	ListViewPagesToCache = '3'
+	TimeZone             = 'Pacific Standard Time' #'(GMT-08:00) Pacific Time (US & Canada)'
+	UICacheTime          = '86400'
+	UICountCacheTime     = '600'
+	UIUserCacheTime      = '14400'
+	DependsOn            = '[MimSvcTimeZoneConfiguration]GMT0800PacificTimeUSCanada'
+	Ensure               = 'Present'
+}
+
+Test-MimSvcTargetResource -ObjectType Person -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+Set-MimSvcTargetResource -ObjectType Person -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+#endregion
+
+#region: Test for an existing PortalUIConfiguration
+Configuration TestPortalUIConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        PortalUIConfiguration 'My Profile'
+        {
+            BrandingLeftImage           = '~/_layouts/images/MSILM2/logo.png'
+            BrandingRightImage          = '~/_layouts/images/MSILM2/banner.png'
+            DisplayName                 = 'Portal Configuration'
+            IsConfigurationType         = $false
+            ListViewCacheTimeOut        = 120
+            ListViewPageSize            = 30
+            ListViewPagesToCache        = 3
+            TimeZone                    = 'Pacific Standard Time'
+            UICacheTime                 = 86400
+            UICountCacheTime            = 600
+            UIUserCacheTime             = 14400
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestPortalUIConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestPortalUIConfiguration" -Force
+#endregion
+
+#region: New PortalUIConfiguration
+$randomNumber = Get-Random -Minimum 100 -Maximum 999
+Configuration TestPortalUIConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        PortalUIConfiguration "TestPortalUIConfiguration$randomNumber"
+        {
+            BrandingLeftImage           = '~/_layouts/images/MSILM2/logo.png'
+            BrandingRightImage          = '~/_layouts/images/MSILM2/banner.png'
+            DisplayName                 = "TestPortalUIConfiguration$randomNumber"
+            IsConfigurationType         = $false
+            ListViewCacheTimeOut        = 120
+            ListViewPageSize            = 30
+            ListViewPagesToCache        = 3
+            TimeZone                    = 'Pacific Standard Time'
+            UICacheTime                 = 86400
+            UICountCacheTime            = 600
+            UIUserCacheTime             = 14400
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestPortalUIConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestPortalUIConfiguration" -Force
+#endregion
+
+#region: Update PortalUIConfiguration
+$randomNumber = Get-Random -Minimum 100 -Maximum 999
+Configuration TestPortalUIConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        PortalUIConfiguration "TestPortalUIConfiguration$randomNumber"
+        {
+            BrandingLeftImage           = '~/_layouts/images/MSILM2/logo.png'
+            BrandingRightImage          = '~/_layouts/images/MSILM2/banner.png'
+            DisplayName                 = "TestPortalUIConfiguration$randomNumber"
+            IsConfigurationType         = $false
+            ListViewCacheTimeOut        = 120
+            ListViewPageSize            = 30
+            ListViewPagesToCache        = 3
+            TimeZone                    = 'Pacific Standard Time'
+            UICacheTime                 = 86400
+            UICountCacheTime            = 600
+            UIUserCacheTime             = 14400
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestPortalUIConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestPortalUIConfiguration" -Force
+
+Configuration TestPortalUIConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        PortalUIConfiguration "TestPortalUIConfiguration$randomNumber"
+        {
+            BrandingLeftImage           = '~/_layouts/images/MSILM2/logo.png'
+            BrandingRightImage          = '~/_layouts/images/MSILM2/banner.png'
+            DisplayName                 = "TestPortalUIConfiguration$randomNumber"
+            IsConfigurationType         = $false
+            ListViewCacheTimeOut        = 121
+            ListViewPageSize            = 31
+            ListViewPagesToCache        = 2
+            TimeZone                    = 'Pacific Standard Time'
+            UICacheTime                 = 86400
+            UICountCacheTime            = 600
+            UIUserCacheTime             = 14400
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestPortalUIConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestPortalUIConfiguration" -Force
+#endregion
---
+++ DscResources/MimSvcPortalUIConfiguration/MimSvcPortalUIConfiguration.psm1
@@ -0,0 +1,194 @@
+﻿function Get-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Collections.Hashtable])]
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName
+	)
+
+	#Write-Verbose "Use this cmdlet to deliver information about command processing."
+
+	#Write-Debug "Use this cmdlet to write debug information while troubleshooting."
+
+
+	<#
+	$returnValue = @{
+		BrandingCenterText = [System.String]
+		BrandingLeftImage = [System.String]
+		BrandingRightImage = [System.String]
+		CreatedTime = [System.DateTime]
+		Creator = [System.String]
+		DeletedTime = [System.DateTime]
+		Description = [System.String]
+		DetectedRulesList = [System.String[]]
+		DisplayName = [System.String]
+		ExpectedRulesList = [System.String[]]
+		ExpirationTime = [System.DateTime]
+		UICacheTime = [System.UInt64]
+		IsConfigurationType = [System.Boolean]
+		ListViewCacheTimeOut = [System.UInt64]
+		ListViewPageSize = [System.UInt64]
+		ListViewPagesToCache = [System.UInt64]
+		Locale = [System.String]
+		MVObjectID = [System.String]
+		UICountCacheTime = [System.UInt64]
+		UIUserCacheTime = [System.UInt64]
+		ObjectID = [System.String]
+		ResourceTime = [System.DateTime]
+		ObjectType = [System.String]
+		TimeZone = [System.String]
+		Credential = [System.Management.Automation.PSCredential]
+		Ensure = [System.String]
+	}
+
+	$returnValue
+	#>
+}
+
+function Set-TargetResource
+{
+	[CmdletBinding()]
+	param
+	(
+		[System.String]
+		$BrandingCenterText,
+
+		[System.String]
+		$BrandingLeftImage,
+
+		[System.String]
+		$BrandingRightImage,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.UInt64]
+		$UICacheTime,
+
+		[System.Boolean]
+		$IsConfigurationType,
+
+		[System.UInt64]
+		$ListViewCacheTimeOut,
+
+		[System.UInt64]
+		$ListViewPageSize,
+
+		[System.UInt64]
+		$ListViewPagesToCache,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.UInt64]
+		$UICountCacheTime,
+
+		[System.UInt64]
+		$UIUserCacheTime,
+
+		[System.String]
+		$TimeZone,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Set-MimSvcTargetResource -ObjectType PortalUIConfiguration -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+function Test-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Boolean])]
+	param
+	(
+		[System.String]
+		$BrandingCenterText,
+
+		[System.String]
+		$BrandingLeftImage,
+
+		[System.String]
+		$BrandingRightImage,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.UInt64]
+		$UICacheTime,
+
+		[System.Boolean]
+		$IsConfigurationType,
+
+		[System.UInt64]
+		$ListViewCacheTimeOut,
+
+		[System.UInt64]
+		$ListViewPageSize,
+
+		[System.UInt64]
+		$ListViewPagesToCache,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.UInt64]
+		$UICountCacheTime,
+
+		[System.UInt64]
+		$UIUserCacheTime,
+
+		[System.String]
+		$TimeZone,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Test-MimSvcTargetResource -ObjectType PortalUIConfiguration -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+Export-ModuleMember -Function *-TargetResource
