e295bf2a87644f0a3be53f72f219c663cf3829fc
craig-martin <craigmartin@live.com>
2024-04-30 20:40:18 +0000

    adds Resource DSC resource

---
+++ DscExamples/Test-Resource.ps1
@@ -0,0 +1,44 @@
+﻿
+Set-Location c:\MimDsc
+
+#region Call the Set and Test methods directly - allows for setting a breakpoint for debuggering
+Search-Resources -XPath "/Resource" -ExpectedObjectType Resource -MaxResults 5
+
+ipmo MimDsc -Force
+ipmo 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcResource\MimSvcResource.psm1' -Force
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcResource\MimSvcResource.psm1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Test-MimSvcTargetResource.ps1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Set-MimSvcTargetResource.ps1'
+
+$resourceProperties = @{
+	DisplayName = 'Anonymous User'
+    Ensure		= 'Present'
+}
+
+Test-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+Set-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+#endregion
+
+#region: Test for an existing Resource
+
+Configuration TestResource 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        Resource AnonymousUser
+        {
+            DisplayName = 'Anonymous User'
+            Ensure		= 'Present'
+        }
+    } 
+} 
+
+TestResource
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestResource" -Force
+#endregion
+
---
+++ DscResources/MimSvcResource/MimSvcResource.psm1
@@ -0,0 +1,118 @@
+﻿function Get-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Collections.Hashtable])]
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName
+	)
+
+	#Write-Verbose "Use this cmdlet to deliver information about command processing."
+
+	#Write-Debug "Use this cmdlet to write debug information while troubleshooting."
+
+
+	<#
+	$returnValue = @{
+		CreatedTime = [System.DateTime]
+		Creator = [System.String]
+		DeletedTime = [System.DateTime]
+		Description = [System.String]
+		DetectedRulesList = [System.String[]]
+		DisplayName = [System.String]
+		ExpectedRulesList = [System.String[]]
+		ExpirationTime = [System.DateTime]
+		Locale = [System.String]
+		MVObjectID = [System.String]
+		ObjectID = [System.String]
+		ResourceTime = [System.DateTime]
+		ObjectType = [System.String]
+		Credential = [System.Management.Automation.PSCredential]
+		Ensure = [System.String]
+	}
+
+	$returnValue
+	#>
+}
+
+function Set-TargetResource
+{
+	[CmdletBinding()]
+	param
+	(
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Set-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+function Test-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Boolean])]
+	param
+	(
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+	Test-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+
+}
+
+Export-ModuleMember -Function *-TargetResource
