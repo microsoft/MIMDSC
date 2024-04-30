76a22008bba790c0adbce2c2de962b24dbbceb6d
craig-martin <craigmartin@live.com>
2024-04-30 20:50:39 +0000

    adds SearchScopeConfiguration DSC resource

---
+++ DscExamples/Test-SearchScopeConfiguration.ps1
@@ -0,0 +1,124 @@
+﻿Set-Location c:\MimDsc
+
+#region Call the Set and Test methods directly - allows for setting a breakpoint for debuggering
+Search-Resources -XPath "/SearchScopeConfiguration" -ExpectedObjectType SearchScopeConfiguration -MaxResults 5
+
+ipmo MimDsc -Force
+ipmo 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcSearchScopeConfiguration\MimSvcSearchScopeConfiguration.psm1' -Force
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\DSCResources\MimSvcSearchScopeConfiguration\MimSvcSearchScopeConfiguration.psm1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Test-MimSvcTargetResource.ps1'
+psedit 'C:\Program Files\WindowsPowerShell\Modules\MimDsc\Functions\Set-MimSvcTargetResource.ps1'
+
+$resourceProperties = @{
+    DisplayName                 = 'All Mail Enabled Security Groups'
+    IsConfigurationType         = $true
+    Order                       = 30
+    SearchScope                 = "/Group[Type='MailEnabledSecurity']"
+    SearchScopeColumn           = 'DisplayName;Description;Email'
+    SearchScopeContext          = 'DisplayName', 'AccountName'
+    SearchScopeResultObjectType = 'Group'
+    UsageKeyword                = 'customized', 'Distribution'
+    Ensure						= 'Present'
+}
+
+Test-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+Set-MimSvcTargetResource -ObjectType Resource -KeyAttributeName DisplayName -DscBoundParameters $resourceProperties -Verbose:$true
+
+#endregion
+
+#region: Test for an existing SearchScopeConfiguration
+Search-Resources -XPath  "/SearchScopeConfiguration[DisplayName='All Mail Enabled Security Groups']" -ExpectedObjectType SearchScopeConfiguration
+Configuration TestSearchScopeConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        SearchScopeConfiguration 'All Mail Enabled Security Groups'
+        {
+            DisplayName                 = 'All Mail Enabled Security Groups'
+            IsConfigurationType         = $true
+            Order                       = 30
+            SearchScope                 = "/Group[Type='MailEnabledSecurity']"
+            SearchScopeColumn           = 'DisplayName;Description;Email'
+            SearchScopeContext          = 'DisplayName', 'AccountName'
+            SearchScopeResultObjectType = 'Group'
+            UsageKeyword                = 'customized', 'Distribution'
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestSearchScopeConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Path "C:\MimDsc\TestSearchScopeConfiguration" -Force
+#endregion
+
+#region: New SearchScopeConfiguration
+$randomNumber = Get-Random -Minimum 1000 -Maximum 9999
+Configuration TestSearchScopeConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        SearchScopeConfiguration "TestSearchScopeConfiguration$randomNumber"
+        {
+            DisplayName                 = "TestSearchScopeConfiguration$randomNumber"
+            IsConfigurationType         = $false
+            Order                       = 1201
+            SearchScope                 = "/Group[Type='MailEnabledSecurity']"
+            SearchScopeColumn           = 'DisplayName;Description;Email'
+            SearchScopeContext          = 'DisplayName', 'AccountName'
+            SearchScopeResultObjectType = 'Group'
+            UsageKeyword                = 'customized', 'Distribution'
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestSearchScopeConfiguration
+
+Start-DscConfiguration -Wait -Debug -Verbose -Path "C:\MimDsc\TestSearchScopeConfiguration" -Force
+#endregion
+
+#region: Update SearchScopeConfiguration
+$randomNumber = Get-Random -Minimum 1000 -Maximum 9999
+
+$searchScopeConfiguration = New-Resource -ObjectType SearchScopeConfiguration
+$searchScopeConfiguration.DisplayName                 = "TestSearchScopeConfiguration$randomNumber"
+$searchScopeConfiguration.IsConfigurationType         = $true
+$searchScopeConfiguration.Order                       = $randomNumber
+$searchScopeConfiguration.SearchScope                 = "/Group[Type='MailEnabledSecurity']"
+$searchScopeConfiguration.SearchScopeColumn           = 'DisplayName;Description;Email;AccountName'
+$searchScopeConfiguration.SearchScopeContext          = 'DisplayName', 'AccountName', 'Email'
+$searchScopeConfiguration.SearchScopeResultObjectType = 'Group'
+$searchScopeConfiguration.UsageKeyword                = 'customized', 'Distribution'
+$searchScopeConfiguration | Save-Resource
+
+Configuration TestSearchScopeConfiguration 
+{ 
+    Import-DscResource -ModuleName MimDsc
+
+    Node (hostname) 
+    { 
+        SearchScopeConfiguration "TestSearchScopeConfiguration$randomNumber"
+        {
+            DisplayName                 = "TestSearchScopeConfiguration$randomNumber"
+            IsConfigurationType         = $true
+            Order                       = $randomNumber
+            SearchScope                 = "/Group[Type='MailEnabledSecurity']"
+            SearchScopeColumn           = 'DisplayName;Description;Email;AccountName'
+            SearchScopeContext          = 'DisplayName', 'AccountName', 'Email'
+            SearchScopeResultObjectType = 'Group'
+            UsageKeyword                = 'customized', 'Distribution'
+            Ensure						= 'Present'
+        }
+    } 
+} 
+
+TestSearchScopeConfiguration
+
+Start-DscConfiguration -Wait -Verbose -Debug -Path "C:\MimDsc\TestSearchScopeConfiguration" -Force
+#endregion
---
+++ DscResources/MimSvcSearchScopeConfiguration/MimSvcSearchScopeConfiguration.psm1
@@ -0,0 +1,194 @@
+﻿function Get-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Collections.Hashtable])]
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+        [parameter(Mandatory = $true)]
+		[System.UInt64]
+		$Order
+	)
+
+	#Write-Verbose "Use this cmdlet to deliver information about command processing."
+
+	#Write-Debug "Use this cmdlet to write debug information while troubleshooting."
+
+
+	<#
+	$returnValue = @{
+		msidmSearchScopeAdvancedFilter = [System.String]
+		SearchScopeColumn = [System.String]
+		SearchScopeContext = [System.String[]]
+		CreatedTime = [System.DateTime]
+		Creator = [System.String]
+		DeletedTime = [System.DateTime]
+		Description = [System.String]
+		DetectedRulesList = [System.String[]]
+		DisplayName = [System.String]
+		ExpectedRulesList = [System.String[]]
+		ExpirationTime = [System.DateTime]
+		IsConfigurationType = [System.Boolean]
+		Locale = [System.String]
+		MVObjectID = [System.String]
+		NavigationPage = [System.String]
+		Order = [System.UInt64]
+		SearchScopeTargetURL = [System.String]
+		ObjectID = [System.String]
+		ResourceTime = [System.DateTime]
+		ObjectType = [System.String]
+		SearchScopeResultObjectType = [System.String]
+		SearchScope = [System.String]
+		UsageKeyword = [System.String[]]
+		Credential = [System.Management.Automation.PSCredential]
+		Ensure = [System.String]
+	}
+
+	$returnValue
+	#>
+}
+
+function Set-TargetResource
+{
+	[CmdletBinding()]
+	param
+	(
+		[System.String]
+		$msidmSearchScopeAdvancedFilter,
+
+		[System.String]
+		$SearchScopeColumn,
+
+		[System.String[]]
+		$SearchScopeContext,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.Boolean]
+		$IsConfigurationType,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.String]
+		$NavigationPage,
+
+        [parameter(Mandatory = $true)]
+		[System.UInt64]
+		$Order,
+
+		[System.String]
+		$SearchScopeTargetURL,
+
+		[System.String]
+		$SearchScopeResultObjectType,
+
+		[System.String]
+		$SearchScope,
+
+		[System.String[]]
+		$UsageKeyword,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Set-MimSvcTargetResource -ObjectType SearchScopeConfiguration -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+}
+
+function Test-TargetResource
+{
+	[CmdletBinding()]
+	[OutputType([System.Boolean])]
+	param
+	(
+		[System.String]
+		$msidmSearchScopeAdvancedFilter,
+
+		[System.String]
+		$SearchScopeColumn,
+
+		[System.String[]]
+		$SearchScopeContext,
+
+		[System.String]
+		$Description,
+
+		[System.String[]]
+		$DetectedRulesList,
+
+		[parameter(Mandatory = $true)]
+		[System.String]
+		$DisplayName,
+
+		[System.String[]]
+		$ExpectedRulesList,
+
+		[System.DateTime]
+		$ExpirationTime,
+
+		[System.Boolean]
+		$IsConfigurationType,
+
+		[System.String]
+		$Locale,
+
+		[System.String]
+		$MVObjectID,
+
+		[System.String]
+		$NavigationPage,
+
+        [parameter(Mandatory = $true)]
+		[System.UInt64]
+		$Order,
+
+		[System.String]
+		$SearchScopeTargetURL,
+
+		[System.String]
+		$SearchScopeResultObjectType,
+
+		[System.String]
+		$SearchScope,
+
+		[System.String[]]
+		$UsageKeyword,
+
+		[System.Management.Automation.PSCredential]
+		$Credential,
+
+		[ValidateSet("Present","Absent")]
+		[System.String]
+		$Ensure
+	)
+
+    Test-MimSvcTargetResource -ObjectType SearchScopeConfiguration -KeyAttributeName DisplayName -DscBoundParameters $PSBoundParameters
+
+}
+
+Export-ModuleMember -Function *-TargetResource
